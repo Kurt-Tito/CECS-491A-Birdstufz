513eb6ecb3ba0cdccedeb50a518966e67642906e
Kurt Tito <kurtcedricktito@gmail.com>
2018-02-13 19:36:03 -0800

    Initializing project

---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//DELETE CODE BELOW
+		//This is just to initialize the development of the project
+		
+		System.out.println("Initializing...");
+
+	}
+
+}
