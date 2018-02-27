4061e1d6a3a907c3585a23e1547471a568f8790a
DESKTOP-JTSHV9T\Justin <justin.le825@gmail.com>
2018-02-27 14:28:21 -0800

    Fixed Maze background

--- src/MazePanel.java
+++ src/MazePanel.java
@@ -31,6 +31,7 @@ public class MazePanel extends JPanel implements MouseListener, KeyListener{
 	
 	public void paintComponent(Graphics g)
 	{
+		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		
 		maze.drawMaze(g2, 0, 0, 64);
