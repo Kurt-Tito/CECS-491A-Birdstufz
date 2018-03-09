485ed3a275ba9d6ca6bb949bd90ff1226c3c4961
Kurt Tito <kurtcedricktito@gmail.com>
2018-03-08 21:37:13 -0800

    Finished king piece move implementation

--- src/ChessKing.java
+++ src/ChessKing.java
@@ -39,7 +39,135 @@ public class ChessKing extends ChessPiece{
 	@Override
 	public List<ChessTile> getValidMoves(ChessBoard board, ChessTile location) {
 		List<ChessTile> validMoves = new ArrayList<ChessTile>();
-		
+		ChessTile tile;
+		//north
+		if(location.getRow() > 0)
+		{
+			tile = board.getBoard()[location.getCol()][location.getRow()-1];
+			if(!tile.hasPiece())
+			{
+				validMoves.add(tile);
+			}
+			if(tile.hasPiece())
+			{
+				if(colorAlignment != tile.getPiece().getColorAlignment())
+				{
+					validMoves.add(tile);
+				}
+			}
+		}
+		//south
+		if(location.getRow() < board.getBoard().length - 1)
+		{
+			tile = board.getBoard()[location.getCol()][location.getRow()+1];
+			if(!tile.hasPiece())
+			{
+				validMoves.add(tile);
+			}
+			if(tile.hasPiece())
+			{
+				if(colorAlignment != tile.getPiece().getColorAlignment())
+				{
+					validMoves.add(tile);
+				}
+			}
+		}
+		//west
+		if(location.getCol() > 0)
+		{
+			tile = board.getBoard()[location.getCol()-1][location.getRow()];
+			if(!tile.hasPiece())
+			{
+				validMoves.add(tile);
+			}
+			if(tile.hasPiece())
+			{
+				if(colorAlignment != tile.getPiece().getColorAlignment())
+				{
+					validMoves.add(tile);
+				}
+			}
+		}
+		//east
+		if(location.getCol() < board.getBoard().length - 1)
+		{
+			tile = board.getBoard()[location.getCol()+1][location.getRow()];
+			if(!tile.hasPiece())
+			{
+				validMoves.add(tile);
+			}
+			if(tile.hasPiece())
+			{
+				if(colorAlignment != tile.getPiece().getColorAlignment())
+				{
+					validMoves.add(tile);
+				}
+			}
+		}
+		//north west
+		if(location.getRow() > 0 && location.getCol() > 0)
+		{
+			tile = board.getBoard()[location.getCol()-1][location.getRow()-1];
+			if(!tile.hasPiece())
+			{
+				validMoves.add(tile);
+			}
+			if(tile.hasPiece())
+			{
+				if(colorAlignment != tile.getPiece().getColorAlignment())
+				{
+					validMoves.add(tile);
+				}
+			}
+		}
+		//north east
+		if(location.getRow() > 0 && location.getCol() < board.getBoard().length - 1)
+		{
+			tile = board.getBoard()[location.getCol()+1][location.getRow()-1];
+			if(!tile.hasPiece())
+			{
+				validMoves.add(tile);
+			}
+			if(tile.hasPiece())
+			{
+				if(colorAlignment != tile.getPiece().getColorAlignment())
+				{
+					validMoves.add(tile);
+				}
+			}
+		}
+		//south west
+		if(location.getRow() < board.getBoard().length - 1 && location.getCol() > 0)
+		{
+			tile = board.getBoard()[location.getCol()-1][location.getRow()+1];
+			if(!tile.hasPiece())
+			{
+				validMoves.add(tile);
+			}
+			if(tile.hasPiece())
+			{
+				if(colorAlignment != tile.getPiece().getColorAlignment())
+				{
+					validMoves.add(tile);
+				}
+			}
+		}
+		//south east
+		if(location.getRow() < board.getBoard().length - 1 && location.getCol() < board.getBoard().length - 1)
+		{
+			tile = board.getBoard()[location.getCol()+1][location.getRow()+1];
+			if(!tile.hasPiece())
+			{
+				validMoves.add(tile);
+			}
+			if(tile.hasPiece())
+			{
+				if(colorAlignment != tile.getPiece().getColorAlignment())
+				{
+					validMoves.add(tile);
+				}
+			}
+		}
 		// TODO Auto-generated method stub
 		return validMoves;
 	}
