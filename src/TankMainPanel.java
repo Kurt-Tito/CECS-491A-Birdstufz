164d0e6fbd54d07cf91197bc35b528ae3cd98895
DESKTOP-JTSHV9T\Justin <justin.le825@gmail.com>
2018-04-17 14:38:39 -0700

    Adjusted maze design

--- src/TankMainPanel.java
+++ src/TankMainPanel.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,13 +11,20 @@ import javax.swing.JPanel;
 public class TankMainPanel extends JPanel{
 	private final int TILE_SIZE = 90;
 	private final int WALL_THICKNESS = 10;
-	private int size;
+	private int rows, cols;
 	private TankMaze maze;
+	private ArrayList<Rectangle> walls = new ArrayList<Rectangle>();
 	public TankMainPanel()
 	{
-		setPreferredSize(new Dimension(990, 990));
+		setPreferredSize(new Dimension(990, 810));
 		maze = new TankMaze();
-		size = maze.size;
+		rows = maze.height;
+		cols = maze.width;
+		ArrayList<Edge> temp = maze.getWalls();
+		for(int i = 0; i < temp.size(); i++)
+		{
+			walls.add(new TankMazeWall(temp.get(i), TILE_SIZE, WALL_THICKNESS));
+		}
 	}
 	@Override
 	public void paintComponent(Graphics g)
@@ -23,7 +32,7 @@ public class TankMainPanel extends JPanel{
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(Color.black);
 		
-		for(int i = 0; i < size; i++)
+		for(int i = 0; i < cols; i++)
 		{
 			//g2.drawLine(i* TILE_SIZE, 0, i* TILE_SIZE + 75, 0);
 			//g2.drawLine(i* TILE_SIZE, TILE_SIZE * size, i* TILE_SIZE + 75, TILE_SIZE * size);
@@ -31,12 +40,16 @@ public class TankMainPanel extends JPanel{
 			//g2.drawLine(TILE_SIZE * size, i* TILE_SIZE, TILE_SIZE * size, i* TILE_SIZE + TILE_SIZE);
 			
 			g2.fillRect(i*TILE_SIZE - WALL_THICKNESS/2, 0 - WALL_THICKNESS/2, TILE_SIZE + WALL_THICKNESS/2, WALL_THICKNESS);
-			g2.fillRect(i*TILE_SIZE - WALL_THICKNESS/2, TILE_SIZE * size - WALL_THICKNESS/2, TILE_SIZE + WALL_THICKNESS, WALL_THICKNESS);
+			g2.fillRect(i*TILE_SIZE - WALL_THICKNESS/2, TILE_SIZE * rows - WALL_THICKNESS/2, TILE_SIZE + WALL_THICKNESS, WALL_THICKNESS);
 			
-			g2.fillRect(0 - WALL_THICKNESS/2, i*TILE_SIZE - WALL_THICKNESS/2, WALL_THICKNESS, TILE_SIZE + WALL_THICKNESS/2);
-			g2.fillRect(TILE_SIZE * size - WALL_THICKNESS/2, i*TILE_SIZE - WALL_THICKNESS/2, WALL_THICKNESS, TILE_SIZE + WALL_THICKNESS);
+			
 		}
-		
+		for(int i = 0; i < rows; i++)
+		{
+			g2.fillRect(0 - WALL_THICKNESS/2, i*TILE_SIZE - WALL_THICKNESS/2, WALL_THICKNESS, TILE_SIZE + WALL_THICKNESS/2);
+			g2.fillRect(TILE_SIZE * cols - WALL_THICKNESS/2, i*TILE_SIZE - WALL_THICKNESS/2, WALL_THICKNESS, TILE_SIZE + WALL_THICKNESS);
+		}
+		/**
 		for(int i = 0; i < maze.walls.size(); i++)
 		{
 			Edge wall = maze.walls.get(i);
@@ -59,6 +72,12 @@ public class TankMainPanel extends JPanel{
 				//g2.drawLine((wall.from.getX()+1) * TILE_SIZE, wall.from.getY()* TILE_SIZE, (wall.from.getX()+1)* TILE_SIZE, wall.to.getY()* TILE_SIZE + TILE_SIZE);
 			}
 			
+		}
+		*/
+		for(int i = 0; i < walls.size(); i++)
+		{
+			Rectangle r = walls.get(i);
+			g2.fillRect(r.x, r.y, r.width, r.height);
 		}
 		
 	}
--- src/TankMaze.java
+++ src/TankMaze.java
@@ -1,26 +1,27 @@
-import java.awt.Point;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.Random;
 
 public class TankMaze {
-	public int size;
+	public int width, height;
 	private ArrayList<Edge> paths;
-	public ArrayList<Edge> walls;
+	private ArrayList<Edge> walls;
 	public ArrayList<Node> nodes;
 	public TankMaze()
 	{
-		size = 11;
+		width = 11;
+		height = 9;
 		nodes= new ArrayList<Node>();
 		paths = new ArrayList<Edge>();
 		walls = new ArrayList<Edge>();
 		
-		initializeMaze();
+		resetMaze();
 		
+	}
+	
+	public ArrayList<Edge> getWalls()
+	{
+		return walls;
 	}
 	
 	private Node getNode(int x, int y)
@@ -29,26 +30,34 @@ public class TankMaze {
 		return n;
 	}
 	
+	public void resetMaze()
+	{
+		nodes.clear();
+		paths.clear();
+		walls.clear();
+		initializeMaze();
+	}
+	
 	public void initializeMaze()
 	{
-		for(int i = 0; i < size; i++)	//Add nodes
+		for(int i = 0; i < height; i++)	//Add nodes
 		{
-			for(int j = 0; j < size; j++) {
+			for(int j = 0; j < width; j++) {
 				Node node = new Node(j,i);
 				nodes.add(node);
 			}
 		}
 		
-		for(int i = 0; i < size; i++)	//Add wall segments between all nodes
+		for(int i = 0; i < height; i++)	//Add wall segments between all nodes
 		{
-			for(int j = 0; j < size; j++) {
-				if(j < size-1)
+			for(int j = 0; j < width; j++) {
+				if(j < width-1)
 				{
 					Node n1 = getNode(j,i);
 					Node n2 = getNode(j+1,i);
 					walls.add(new Edge(n1,n2));
 				}
-				if(i < size-1)
+				if(i < height-1)
 				{
 					Node n1 = getNode(j,i);
 					Node n2 = getNode(j,i+1);
@@ -62,14 +71,14 @@ public class TankMaze {
 		addOuterCorridors();
 		
 		
-		ArrayList<Node> area1 = getMazeArea(1,1,4,4);	//Generate 4 inner maze areas
-		ArrayList<Node> area2 = getMazeArea(6,6,9,9);
-		ArrayList<Node> area3 = getMazeArea(1,6,4,9);
-		ArrayList<Node> area4 = getMazeArea(6,1,9,4);
-		area1.remove(new Node(4,4));
-		area2.remove(new Node(6,6));
-		area3.remove(new Node(4,6));
-		area4.remove(new Node(6,4));
+		ArrayList<Node> area1 = getMazeArea(1,1,4,3);	//Generate 4 inner maze areas
+		ArrayList<Node> area2 = getMazeArea(6,5,9,7);
+		ArrayList<Node> area3 = getMazeArea(1,5,4,7);
+		ArrayList<Node> area4 = getMazeArea(6,1,9,3);
+		area1.remove(new Node(4,3));
+		area2.remove(new Node(6,5));
+		area3.remove(new Node(4,5));
+		area4.remove(new Node(6,3));
 		generateMazeArea(area1);
 		generateMazeArea(area2);
 		generateMazeArea(area3);
@@ -79,17 +88,17 @@ public class TankMaze {
 	}
 	private void addCenter()
 	{
-		for(int i = 4; i < size - 4; i++)	//Remove center square walls
+		for(int i = height/2 - 1; i < height/2 + 2; i++)	//Remove center square walls
 		{
-			for(int j = 4; j < size-4; j++)
+			for(int j = width/2 - 1; j < width/2 + 2; j++)
 			{
-				if(j < size-5)
+				if(j < width-5)
 				{
 					Node n1 = getNode(j,i);
 					Node n2 = getNode(j+1,i);
 					walls.remove(new Edge(n1,n2));
 				}
-				if(i < size-5)
+				if(i < height-4)
 				{
 					Node n1 = getNode(j,i);
 					Node n2 = getNode(j,i+1);
@@ -101,7 +110,7 @@ public class TankMaze {
 	
 	private void addInnerCorridors()
 	{
-		for(int i = 0; i < size - 1; i++)	//Remove center corridor walls
+		for(int i = 0; i < height - 1; i++)	//Remove center corridor walls
 		{
 			
 				Node node1, node2;
@@ -121,34 +130,44 @@ public class TankMaze {
 				walls.remove(new Edge(node1, node2));
 				*/
 				
-				node1 = getNode(i, 5);
-				node2 = getNode(i+1, 5);
+				
+				node1 = getNode(width / 2,i);
+				node2 = getNode(width / 2, i+1);
 				walls.remove(new Edge(node1, node2));
-				node1 = getNode(5,i);
-				node2 = getNode(5, i+1);
-				walls.remove(new Edge(node1, node2));
+		}
+		for(int i = 0; i < width - 1; i++)
+		{
+			Node node1, node2;
+			node1 = getNode(i, height / 2);
+			node2 = getNode(i+1, height / 2);
+			walls.remove(new Edge(node1, node2));
 		}
 	}
 	
 	private void addOuterCorridors()
 	{
-		for(int i = 0; i < size - 1; i++)	//Remove outer corridor walls
+		for(int i = 0; i < height - 1; i++)	//Remove outer corridor walls
+		{
+			Node node1, node2;
+			
+			
+			node1 = getNode(0,i);
+			node2 = getNode(0, i+1);
+			walls.remove(new Edge(node1, node2));
+			
+			node1 = getNode(width-1,i);
+			node2 = getNode(width-1, i+1);
+			walls.remove(new Edge(node1, node2));
+		}
+		for(int i = 0; i < width - 1; i++)
 		{
 			Node node1, node2;
 			node1 = getNode(i,0);
 			node2 = getNode(i+1, 0);
 			walls.remove(new Edge(node1, node2));
 			
-			node1 = getNode(i,size-1);
-			node2 = getNode(i+1, size-1);
-			walls.remove(new Edge(node1, node2));
-			
-			node1 = getNode(0,i);
-			node2 = getNode(0, i+1);
-			walls.remove(new Edge(node1, node2));
-			
-			node1 = getNode(size-1,i);
-			node2 = getNode(size-1, i+1);
+			node1 = getNode(i,height-1);
+			node2 = getNode(i+1, height-1);
 			walls.remove(new Edge(node1, node2));
 		}
 	}
@@ -157,58 +176,68 @@ public class TankMaze {
 	{
 		Random random = new Random(System.nanoTime());
 		int rnum, num1, num2;
+		//outer-top
 		rnum = random.nextInt(4);
 		num1 = rnum + 1; num2 = 0;
 		walls.remove(new Edge(getNode(num1,num2),getNode(num1, num2+1)));
 		rnum = random.nextInt(4);
 		num1 = rnum + 6; num2 = 0;
 		walls.remove(new Edge(getNode(num1,num2),getNode(num1, num2+1)));
+		//outer-bottom
 		rnum = random.nextInt(4);
-		num1 = rnum + 1; num2 = size-2;
+		num1 = rnum + 1; num2 = height-2;
 		walls.remove(new Edge(getNode(num1,num2),getNode(num1, num2+1)));
 		rnum = random.nextInt(4);
-		num1 = rnum + 6; num2 = size-2;
+		num1 = rnum + 6; num2 = height-2;
 		walls.remove(new Edge(getNode(num1,num2),getNode(num1, num2+1)));
 		
-		rnum = random.nextInt(4);
+		rnum = random.nextInt(3);
+		//outer left
 		num1 = rnum + 1; num2 = 0;
 		walls.remove(new Edge(getNode(num2,num1),getNode(num2+1, num1)));
-		rnum = random.nextInt(4);
-		num1 = rnum + 6; num2 = 0;
+		rnum = random.nextInt(3);
+		num1 = rnum + 5; num2 = 0;
 		walls.remove(new Edge(getNode(num2,num1),getNode(num2+1, num1)));
-		rnum = random.nextInt(4);
-		num1 = rnum + 1; num2 = size-2;
+		//outer-right
+		rnum = random.nextInt(3);
+		num1 = rnum + 1; num2 = width-2;
 		walls.remove(new Edge(getNode(num2,num1),getNode(num2+1, num1)));
-		rnum = random.nextInt(4);
-		num1 = rnum + 6; num2 = size-2;
+		rnum = random.nextInt(3);
+		num1 = rnum + 5; num2 = width-2;
 		walls.remove(new Edge(getNode(num2,num1),getNode(num2+1, num1)));
 		
+		//inner-top
 		rnum = random.nextInt(3);
-		num1 = rnum + 1; num2 = 4;
+		num1 = rnum + 1; num2 = 3;
 		walls.remove(new Edge(getNode(num1,num2),getNode(num1, num2+1)));
 		rnum = random.nextInt(3);
-		num1 = rnum + 7; num2 = 4;
+		num1 = rnum + 7; num2 = 3;
+		walls.remove(new Edge(getNode(num1,num2),getNode(num1, num2+1)));
+		//inner-bottom
+		rnum = random.nextInt(3);
+		num1 = rnum + 1; num2 = height-5;
 		walls.remove(new Edge(getNode(num1,num2),getNode(num1, num2+1)));
 		rnum = random.nextInt(3);
-		num1 = rnum + 1; num2 = size-6;
-		walls.remove(new Edge(getNode(num1,num2),getNode(num1, num2+1)));
-		rnum = random.nextInt(3);
-		num1 = rnum + 7; num2 = size-6;
+		num1 = rnum + 7; num2 = height-5;
 		walls.remove(new Edge(getNode(num1,num2),getNode(num1, num2+1)));
 		
+		//inner left
 		rnum = random.nextInt(2);
 		num1 = rnum + 1; num2 = 4;
 		walls.remove(new Edge(getNode(num2,num1),getNode(num2+1, num1)));
 		rnum = random.nextInt(2);
-		num1 = rnum + 7; num2 = 4;
+		num1 = rnum + 6; num2 = 4;
+		walls.remove(new Edge(getNode(num2,num1),getNode(num2+1, num1)));
+		//inner right
+		rnum = random.nextInt(2);
+		num1 = rnum + 1; num2 = width-6;
 		walls.remove(new Edge(getNode(num2,num1),getNode(num2+1, num1)));
 		rnum = random.nextInt(2);
-		num1 = rnum + 1; num2 = size-6;
+		num1 = rnum + 6; num2 = width-6;
 		walls.remove(new Edge(getNode(num2,num1),getNode(num2+1, num1)));
-		rnum = random.nextInt(2);
-		num1 = rnum + 7; num2 = size-6;
-		walls.remove(new Edge(getNode(num2,num1),getNode(num2+1, num1)));
+		
 	}
+	
 	
 	/**
 	public void generateMaze()
@@ -378,11 +407,4 @@ public class TankMaze {
 		walls.addAll(mazeWalls);
 	}
 	
-	public static void main(String[] args)
-	{
-		TankMaze maze = new TankMaze();
-		System.out.println(maze.paths);
-		System.out.println(maze.paths.size());
-		System.out.println(maze.walls.size());
-	}
 }
