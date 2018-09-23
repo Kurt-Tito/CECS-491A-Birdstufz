54cf132b514fb1a5cc319d7610a5e536ede45d23
Kurt Tito <kurtcedricktito@gmail.com>
2018-09-23 13:24:47 -0700

    seperated borders from egghuntpanel class and put it in a egghuntarena class

---
+++ src/CECS491B/EggHuntArena.java
@@ -0,0 +1,81 @@
+package CECS491B;
+
+import java.awt.Graphics2D;
+
+public class EggHuntArena {
+	
+	private EggHuntFloor floor = new EggHuntFloor();
+	private EggHuntWalls topborder[] = new EggHuntWalls[25];
+	private EggHuntWalls bottomborder[] = new EggHuntWalls[25];
+	private EggHuntWalls leftborder[] = new EggHuntWalls[13];
+	private EggHuntWalls rightborder[] = new EggHuntWalls[13];
+	
+	int cellSize = 64;
+	int horizontalCells = 25;
+	int verticalCells = 13;
+	
+	public EggHuntArena()
+	{
+		createBorder();
+	}
+	
+	public void createBorder()
+	{
+		for(int i = 0; i < horizontalCells; i++)
+		{
+			topborder[i] = new EggHuntWalls();
+			topborder[i].setLocation(i*cellSize, 0);
+		}
+		
+		/////////////////////////////////////////
+		
+		for(int i = 0; i < horizontalCells; i++)
+		{
+			bottomborder[i] = new EggHuntWalls();
+			bottomborder[i].setLocation(i*cellSize, cellSize*verticalCells);
+		}
+		/////////////////////////////////////////
+		
+		for(int j = 0; j < verticalCells; j++)
+		{
+			leftborder[j] = new EggHuntWalls();
+			leftborder[j].setLocation(0, j*cellSize);
+		}
+		
+		/////////////////////////////////////////
+		
+		for(int j = 0; j < verticalCells; j++)
+		{
+			rightborder[j] = new EggHuntWalls();
+			rightborder[j].setLocation(cellSize*(horizontalCells-1), j*cellSize);
+		}
+	}
+	
+	public void draw(Graphics2D g2)
+	{		
+		//Draw Floor
+		floor.draw(g2);
+		
+		//Draw Borders
+		for(int i = 0; i < horizontalCells; i++)
+		{
+			topborder[i].draw(g2);
+		}
+		
+		for(int i = 0; i < horizontalCells; i++)
+		{
+			bottomborder[i].draw(g2);
+		}
+		
+		for(int i = 0; i < verticalCells; i++)
+		{
+			leftborder[i].draw(g2);
+		}
+		
+		for(int i = 0; i < verticalCells; i++)
+		{
+			rightborder[i].draw(g2);
+		}
+	}
+	
+}
--- src/CECS491B/EggHuntPanel.java
+++ src/CECS491B/EggHuntPanel.java
@@ -9,49 +9,11 @@ import CECS491A.GamePanel;
 
 public class EggHuntPanel extends GamePanel{
 	
-	private EggHuntFloor floor = new EggHuntFloor();
-	private EggHuntWalls topborder[] = new EggHuntWalls[25];
-	private EggHuntWalls bottomborder[] = new EggHuntWalls[25];
-	private EggHuntWalls leftborder[] = new EggHuntWalls[13];
-	private EggHuntWalls rightborder[] = new EggHuntWalls[13];
-	
-	int cellSize = 64;
-	int horizontalCells = 25;
-	int verticalCells = 13;
+	EggHuntArena arena = new EggHuntArena();
 	
 	public EggHuntPanel()
 	{	
 		setPreferredSize(new Dimension(1600, 900));
-		
-		for(int i = 0; i < horizontalCells; i++)
-		{
-			topborder[i] = new EggHuntWalls();
-			topborder[i].setLocation(i*cellSize, 0);
-		}
-		
-		/////////////////////////////////////////
-		
-		for(int i = 0; i < horizontalCells; i++)
-		{
-			bottomborder[i] = new EggHuntWalls();
-			bottomborder[i].setLocation(i*cellSize, cellSize*verticalCells);
-		}
-		/////////////////////////////////////////
-		
-		for(int j = 0; j < verticalCells; j++)
-		{
-			leftborder[j] = new EggHuntWalls();
-			leftborder[j].setLocation(0, j*cellSize);
-		}
-		
-		/////////////////////////////////////////
-		
-		for(int j = 0; j < verticalCells; j++)
-		{
-			rightborder[j] = new EggHuntWalls();
-			rightborder[j].setLocation(cellSize*(horizontalCells-1), j*cellSize);
-		}
-		
 	}
 	
 	@Override
@@ -76,31 +38,7 @@ public class EggHuntPanel extends GamePanel{
 	public void paintComponent(Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D) g;
-		
-		//Draw Floor
-		floor.draw(g2);
-		
-		//Draw Borders
-		for(int i = 0; i < horizontalCells; i++)
-		{
-			topborder[i].draw(g2);
-		}
-		
-		for(int i = 0; i < horizontalCells; i++)
-		{
-			bottomborder[i].draw(g2);
-		}
-		
-		for(int i = 0; i < verticalCells; i++)
-		{
-			leftborder[i].draw(g2);
-		}
-		
-		for(int i = 0; i < verticalCells; i++)
-		{
-			rightborder[i].draw(g2);
-		}
-		
+		arena.draw(g2);
 	}
 
 }
